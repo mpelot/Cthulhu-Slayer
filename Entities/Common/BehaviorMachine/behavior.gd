525c6223d85701fcdade9d8beb031c8c6997f47c
Cody Le <49752325+Cody-Le@users.noreply.github.com>
2024-09-25 14:11:21 -0400

    State Machine and it's extensions
    - Hierarchy Machine
    - State Machine
    - Behavior Machine

---
+++ Entities/Common/BehaviorMachine/behavior.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name Behavior
+
+
+
+func enter():
+	pass
+	
+func exit():
+	pass
+	
+	
+func update(delta):
+	pass
+	
+func physic_update(delta):
+	pass
---
+++ Entities/Common/StateMachine/state.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name State
+
+signal transitioned
+
+func enter(args={}):
+	pass
+	
+func exit():
+	pass
+
+
+	
+func update(delta):
+	pass
+	
+func physic_update(delta):
+	pass
