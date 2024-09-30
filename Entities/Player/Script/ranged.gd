a3cf0ababf1719bd6ffac2ffae0361ce899da57d
g-whittington <105523254+g-whittington@users.noreply.github.com>
2024-09-30 02:25:31 -0400

    Added the four methods of the State class to the HSM child nodes - Melee - Ranged - Idle

--- Entities/Player/Script/ranged.gd
+++ Entities/Player/Script/ranged.gd
@@ -1,1 +1,17 @@
 extends State
+
+
+func enter(_args={}):
+	pass
+	
+func exit():
+	pass
+
+
+#Called every frame
+func update(_delta):
+	pass
+
+#Called every physic frame
+func physic_update(_delta):
+	pass
