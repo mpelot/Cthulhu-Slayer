b630c394d0d604af943b07677e8555f476209c98
Cody Le <49752325+Cody-Le@users.noreply.github.com>
2024-11-04 01:11:39 -0500

    Character Attack Animation (No Colliders)

--- Entities/Player/Script/pivot.gd
+++ Entities/Player/Script/pivot.gd
@@ -1,12 +1,47 @@
 extends Node2D
 class_name Pivot
 
+@onready var attack_sprite: Sprite2D = $Attacks
+@onready var attack_player: AnimationPlayer = $Attacks/AnimationPlayer
 
-func _process(_delta: float):
+signal attack_finished
+
+func _ready():
+	attack_player.animation_finished.connect(animation_finishes)
+
+func attack():
+	visible = true
 	var mouse_direction: Vector2 = get_global_mouse_position() - global_position
-	var compare_direction: Vector2 = Vector2(1,0)
-	rotation = discrete_rotation(compare_direction.angle_to(mouse_direction))
-	
+	var compare_direction: Vector2 = Vector2(0,-1)
+	var angle = discrete_rotation(compare_direction.angle_to(mouse_direction))
+	print(discrete_angle(angle))	
+	set_attack_direction(discrete_angle(angle))
+	run_animation(discrete_angle(angle))
+
+func animation_finishes(anim_name):
+	visible = false
+	attack_finished.emit()
+
+func set_attack_direction(discrete_angle: int):
+	if(discrete_angle != 0):
+		var direction = sign(discrete_angle)
+		attack_sprite.flip_h = direction == -1
+		
+func run_animation(discrete_angle: int):
+	var abs_angle = abs(discrete_angle)
+	if(abs_angle == 0):
+		attack_player.play("up")
+	if(abs_angle == 90):
+		attack_player.play("left")
+	if(abs_angle == 45):
+		attack_player.play("up_right")
+	if(abs_angle == 135):
+		attack_player.play("down_right")
+	if(abs_angle == 180):
+		attack_player.play("down")
+
+func discrete_angle(rad: float):
+	return int(rad_to_deg(rad))
 
 func discrete_rotation(radian: float):
 	return radian - fmod(radian,PI/4)
--- Entities/Player/melee.gd
+++ Entities/Player/melee.gd
@@ -1,17 +1,23 @@
 extends Attack
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var pivot: Pivot = $"../../../Weapon/Pivot"
+@onready var main_body_sprite: Node2D = $"../../../Sprite"
 
+func _ready():
+	pivot.attack_finished.connect(attack_finished)
+	
 
 func enter(_args={}):
 	# Spawn some colliding object
 	print("IS IN MELEE")
-	animation_player.play("melee")
+	pivot.attack()
+	main_body_sprite.visible = false
 	#transitioned.emit("Idle")
 	
 func exit():
 	# Maybe despawn colliding object
-	pass
+	main_body_sprite.visible = true
 
 #Called every frame
 func update(_delta):
@@ -21,6 +27,8 @@ func update(_delta):
 func physic_update(_delta):
 	pass
 
+func attack_finished():
+	attack_finishes.emit(self)
 
 func _on_animation_player_animation_finished(anim_name):
 	if(anim_name == "melee"):
