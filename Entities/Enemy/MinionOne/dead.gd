a960d30d92e2b3128c9b4375eda798e26ac4dccc
Cody Le <49752325+Cody-Le@users.noreply.github.com>
2024-11-13 03:29:40 -0500

    New dead animation

--- Entities/Enemy/MinionOne/dead.gd
+++ Entities/Enemy/MinionOne/dead.gd
@@ -2,8 +2,10 @@ extends State
 
 @onready var cultist: Cultist = $"../.."
 @onready var collider: CollisionShape2D = $"../../CollisionShape2D"
+@onready var animation_player : AnimationPlayer = $"../../AnimationPlayer"
 
 func enter(args = {}):
 	cultist.modulate = Color.BLACK;
 	GameManager.health += 3
+	animation_player.play("die")
 	collider.queue_free()
